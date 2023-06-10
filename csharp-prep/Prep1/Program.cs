5248ff53009954aea1198d3f431496d1d4f3c882
rabbyui <rabbyui2020@gmail.com>
2023-06-11 02:41:46 +0800

    finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,11 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name? ");
-        string first = Console.ReadLine();
+        string first_n = Console.ReadLine();
 
         Console.Write("What is your last name? ");
-        string last = Console.ReadLine();
+        string last_n = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {last}, {first} {last}");
+        Console.WriteLine($"Your name is {last_n}, {first_n} {last_n}");
     }
 }
