ee9041103e942a4336a6b85b6ba064962fd68b60
rabbyui <rabbyui2020@gmail.com>
2023-06-22 11:52:09 +0800

    learning 03 pushed

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine(fraction1.GetFractionString()); // Output: 1/1
+        Console.WriteLine(fraction1.GetDecimalValue()); // Output: 1
+
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine(fraction2.GetFractionString()); // Output: 5/1
+        Console.WriteLine(fraction2.GetDecimalValue()); // Output: 5
+
+        Fraction fraction3 = new Fraction(3, 4);
+        Console.WriteLine(fraction3.GetFractionString()); // Output: 3/4
+        Console.WriteLine(fraction3.GetDecimalValue()); // Output: 0.75
+
+        Fraction fraction4 = new Fraction(1, 3);
+        Console.WriteLine(fraction4.GetFractionString()); // Output: 1/3
+        Console.WriteLine(fraction4.GetDecimalValue()); // Output: 0.3333333333333333
     }
 }
