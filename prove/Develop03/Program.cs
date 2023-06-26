4027e1144f2d14fe23de2517102a038645009376
rabbyui <rabbyui2020@gmail.com>
2023-06-26 11:33:00 +0800

    implemented idea 2 to show creativity and exceeding requirements. See line 13.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -16,7 +16,7 @@ class Program
             new Scripture("Nephi 2:27", "Wherefore, men are free according to the flesh; and all things are given them which are expedient unto man. And they are free to choose liberty and eternal life, through the great Mediator of all men, or to choose captivity and death, according to the captivity and power of the devil; for he seeketh that all men might be hmiserable like unto himself."),
             new Scripture("Nephi 9:28", "O that cunning aplan of the evil one! O the vainness, and the frailties, and the foolishness of men! When they are learned they think they are wise, and they hearken not unto the counsel of God, for they set it aside, supposing they know of themselves, wherefore, their wisdom is foolishness and it profiteth them not. And they shall perish."),
             new Scripture("Nephi 25:23", "For we labor diligently to write, to persuade our children, and also our brethren, to believe in Christ, and to be reconciled to God; for we know that it is by grace that we are saved, after all we can do."),
-            // Add more scriptures to the library
+            // Add more scriptures to the library including scripture mastery
         };
 
         Random random = new Random();
