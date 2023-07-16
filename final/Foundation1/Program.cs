69d8ffc985dc9ebaa1c2745b1fc5bb754102b486
rabbyui <rabbyui2020@gmail.com>
2023-07-16 19:13:07 +0800

    Project Milestone founddation 1

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,131 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+class Comment
+{
+    public string CommenterName { get; set; }
+    public string CommentText { get; set; }
+}
+
+class Video
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Length { get; set; }
+    public List<Comment> Comments { get; }
+
+    public Video()
+    {
+        Comments = new List<Comment>();
+    }
+
+    public int GetCommentCount()
+    {
+        return Comments.Count;
+    }
+}
+
+class VideoManager
+{
+    private List<Video> videos;
+
+    public VideoManager()
+    {
+        videos = new List<Video>();
+    }
+
+    public Video CreateVideo(string title, string author, int length)
+    {
+        Video video = new Video
+        {
+            Title = title,
+            Author = author,
+            Length = length
+        };
+        videos.Add(video);
+        return video;
+    }
+
+    public void AddCommentToVideo(Video video, string commenterName, string commentText)
+    {
+        Comment comment = new Comment
+        {
+            CommenterName = commenterName,
+            CommentText = commentText
+        };
+        video.Comments.Add(comment);
+    }
+
+    public int GetVideoCommentCount(Video video)
+    {
+        return video.GetCommentCount();
+    }
+
+    public string GetVideoInformation(Video video)
+    {
+        string videoInformation = $"Title: {video.Title}\nAuthor: {video.Author}\nLength (seconds): {video.Length}\n";
+        videoInformation += $"Number of Comments: {GetVideoCommentCount(video)}\n";
+        videoInformation += "Comments:\n";
+        foreach (Comment comment in video.Comments)
+        {
+            videoInformation += $"  Commenter: {comment.CommenterName}\n  Comment Text: {comment.CommentText}\n";
+        }
+        return videoInformation;
+    }
+}
+
+class CommentManager
+{
+    public Comment CreateComment(string commenterName, string commentText)
+    {
+        return new Comment
+        {
+            CommenterName = commenterName,
+            CommentText = commentText
+        };
+    }
+
+    public string GetCommentInformation(Comment comment)
+    {
+        return $"Commenter: {comment.CommenterName}\nComment Text: {comment.CommentText}";
+    }
+}
+
+class AbstractionwithYouTubeVideos
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        VideoManager videoManager = new VideoManager();
+        CommentManager commentManager = new CommentManager();
+
+        // Create videos
+        Console.WriteLine();
+        Video video1 = videoManager.CreateVideo("Video 1", "Author 1", 120);
+        Video video2 = videoManager.CreateVideo("Video 2", "Author 2", 180);
+        Video video3 = videoManager.CreateVideo("Video 3", "Author 3", 90);
+
+        // Add comments to videos
+        videoManager.AddCommentToVideo(video1, "User A", "Great video!");
+        videoManager.AddCommentToVideo(video1, "User B", "I learned a lot.");
+        videoManager.AddCommentToVideo(video1, "User C", "Could have been better.");
+
+        videoManager.AddCommentToVideo(video2, "User D", "Very informative!");
+        videoManager.AddCommentToVideo(video2, "User E", "Liked the examples.");
+        videoManager.AddCommentToVideo(video2, "User F", "Loved the examples.");
+
+        videoManager.AddCommentToVideo(video3, "User G", "Short and sweet.");
+        videoManager.AddCommentToVideo(video3, "User I", "Concise and relevant.");
+        videoManager.AddCommentToVideo(video3, "User J", "On-point and impactful.");
+
+        // Display video information and comments
+        List<Video> videos = new List<Video> { video1, video2, video3 };
+        foreach (Video video in videos)
+        {
+            Console.WriteLine(videoManager.GetVideoInformation(video));
+            Console.WriteLine();
+        }
     }
 }
+
+// This program is a YouTube video management system that allows users to create videos, add comments to videos, 
+// and retrieve information about the videos and comments. 
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,166 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+class Order
+{
+    private List<Product> products;
+    private Customer customer;
+    private decimal shippingCost;
+
+    public Order(Customer customer)
+    {
+        products = new List<Product>();
+        this.customer = customer;
+        shippingCost = customer.Address.IsInUSA() ? 5m : 35m;
+    }
+
+    public void AddProduct(Product product)
+    {
+        products.Add(product);
+    }
+
+    public decimal CalculateTotalPrice()
+    {
+        decimal totalPrice = 0;
+        foreach (Product product in products)
+        {
+            totalPrice += product.CalculatePrice();
+        }
+        totalPrice += shippingCost;
+        return totalPrice;
+    }
+
+    public string GetPackingLabel()
+    {
+        string packingLabel = "";
+        foreach (Product product in products)
+        {
+            packingLabel += "Product Name: " + product.Name + ", Product ID: " + product.ProductId + "\n";
+        }
+        return packingLabel;
+    }
+
+    public string GetShippingLabel()
+    {
+        string shippingLabel = "Customer Name: " + customer.Name + "\n";
+        shippingLabel += "Address:\n" + customer.Address.GetFullAddress();
+        return shippingLabel;
+    }
+}
+
+class Product
+{
+    public string Name { get; set; }
+    public int ProductId { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+
+    public decimal CalculatePrice()
+    {
+        return Price * Quantity;
+    }
+}
+
+class Customer
+{
+    public string Name { get; set; }
+    public Address Address { get; set; }
+
+    public bool IsInUSA()
+    {
+        return Address.IsInUSA();
+    }
+}
+
+class Address
+{
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Country { get; set; }
+
+    public bool IsInUSA()
+    {
+        return Country == "USA";
+    }
+
+    public string GetFullAddress()
+    {
+        return Street + "\n" + City + ", " + State + "\n" + Country;
+    }
+}
+
+class EncapsulationwithOnlineOrdering
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        Address address1 = new Address
+        {
+            Street = "123 Main St",
+            City = "New York",
+            State = "NY",
+            Country = "USA"
+        };
+
+        Address address2 = new Address
+        {
+            Street = "456 Elm St",
+            City = "London",
+            State = "N/A",
+            Country = "United Kingdom"
+        };
+
+        Customer customer1 = new Customer
+        {
+            Name = "John Smith",
+            Address = address1
+        };
+
+        Customer customer2 = new Customer
+        {
+            Name = "Jane Doe",
+            Address = address2
+        };
+
+        Product product1 = new Product
+        {
+            Name = "Product 1",
+            ProductId = 1,
+            Price = 10.99m,
+            Quantity = 2
+        };
+
+        Product product2 = new Product
+        {
+            Name = "Product 2",
+            ProductId = 2,
+            Price = 5.99m,
+            Quantity = 3
+        };
+
+        Console.WriteLine();
+        Order order1 = new Order(customer1);
+        order1.AddProduct(product1);
+        order1.AddProduct(product2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(product1);
+        order2.AddProduct(product2);
+
+        Console.WriteLine("Order 1:");
+        Console.WriteLine("Packing Label:\n" + order1.GetPackingLabel());
+        Console.WriteLine("Shipping Label:\n" + order1.GetShippingLabel());
+        Console.WriteLine("Total Price: $" + order1.CalculateTotalPrice());
+        Console.WriteLine();
+
+        Console.WriteLine("Order 2:");
+        Console.WriteLine("Packing Label:\n" + order2.GetPackingLabel());
+        Console.WriteLine("Shipping Label:\n" + order2.GetShippingLabel());
+        Console.WriteLine("Total Price: $" + order2.CalculateTotalPrice());
+        Console.WriteLine();
     }
 }
+
+// This program is an online ordering system for a fitness center. It allows customers to create orders 
+// by selecting products, calculates the total price including shipping costs based on the customer's location, 
+// and generates packing labels and shipping labels for each order. 
