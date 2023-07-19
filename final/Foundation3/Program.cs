86c8005c23d39b7cccb0718c3da680a8229c9fa4
rabbyui <rabbyui2020@gmail.com>
2023-07-19 10:06:51 +0800

    final project

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,9 +1,153 @@
 using System;
 
-class Program
+class Address
+{
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Country { get; set; }
+
+    public string GetFullAddress()
+    {
+        return $"{Street}, {City}, {State}, {Country}";
+    }
+}
+
+class Event
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime Date { get; set; }
+    public TimeSpan Time { get; set; }
+    public Address EventAddress { get; set; }
+
+    public string GetStandardDetails()
+    {
+        string details = $"Title: {Title}\nDescription: {Description}\nDate: {Date.ToShortDateString()}\nTime: {Time.ToString()}\nAddress: {EventAddress.GetFullAddress()}";
+        return details;
+    }
+
+    public virtual string GetFullDetails()
+    {
+        string details = GetStandardDetails();
+        return details;
+    }
+
+    public string GetShortDescription()
+    {
+        string description = $"Type: {GetType().Name}\nTitle: {Title}\nDate: {Date.ToShortDateString()}";
+        return description;
+    }
+}
+
+class Lecture : Event
+{
+    public string Speaker { get; set; }
+    public int Capacity { get; set; }
+
+    public override string GetFullDetails()
+    {
+        string details = base.GetFullDetails();
+        details += $"\nSpeaker: {Speaker}\nCapacity: {Capacity}";
+        return details;
+    }
+}
+
+class Reception : Event
+{
+    public string RSVPEmail { get; set; }
+
+    public override string GetFullDetails()
+    {
+        string details = base.GetFullDetails();
+        details += $"\nRSVP Email: {RSVPEmail}";
+        return details;
+    }
+}
+
+class OutdoorGathering : Event
+{
+    public string WeatherStatement { get; set; }
+
+    public override string GetFullDetails()
+    {
+        string details = base.GetFullDetails();
+        details += $"\nWeather: {WeatherStatement}";
+        return details;
+    }
+}
+
+class InheritancewithEventPlanning
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        Event[] events = new Event[3];
+
+        // Create Lecture event
+        Lecture lecture = new Lecture
+        {
+            Title = "Artificial Intelligence in Healthcare",
+            Description = "Explore the role of AI in revolutionizing healthcare",
+            Date = new DateTime(2023, 7, 15),
+            Time = new TimeSpan(14, 0, 0),
+            EventAddress = new Address
+            {
+                Street = "123 Main St",
+                City = "New York",
+                State = "NY",
+                Country = "USA"
+            },
+            Speaker = "Dr. Jane Smith",
+            Capacity = 100
+        };
+
+        // Create Reception event
+        Reception reception = new Reception
+        {
+            Title = "Networking Mixer",
+            Description = "An evening of networking and socializing",
+            Date = new DateTime(2023, 8, 5),
+            Time = new TimeSpan(18, 30, 0),
+            EventAddress = new Address
+            {
+                Street = "456 Elm St",
+                City = "London",
+                State = "",
+                Country = "United Kingdom"
+            },
+            RSVPEmail = "rsvp@example.com"
+        };
+
+        // Create Outdoor Gathering event
+        OutdoorGathering gathering = new OutdoorGathering
+        {
+            Title = "Summer Music Festival",
+            Description = "An outdoor music festival featuring various artists",
+            Date = new DateTime(2023, 7, 30),
+            Time = new TimeSpan(16, 0, 0),
+            EventAddress = new Address
+            {
+                Street = "789 Oak St",
+                City = "Los Angeles",
+                State = "CA",
+                Country = "USA"
+            },
+            WeatherStatement = "Sunny and warm"
+        };
+
+        events[0] = lecture;
+        events[1] = reception;
+        events[2] = gathering;
+
+        // Display event details
+        foreach (Event evnt in events)
+        {
+            Console.WriteLine("--- Event Details ---");
+            Console.WriteLine(evnt.GetFullDetails());
+            Console.WriteLine();
+        }
     }
 }
+
+// This program is designed to track events for a fitness center. It includes classes to represent  
+// different types of events such as lectures, receptions, and outdoor gatherings. 
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,138 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+abstract class Activity
+{
+    public DateTime Date { get; set; }
+    public int Length { get; set; }
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+    public abstract string GetSummary();
+}
+
+class Running : Activity
+{
+    public double Distance { get; set; }
+
+    public override double GetDistance()
+    {
+        return Distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return (Distance / Length) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return Length / Distance;
+    }
+
+    public override string GetSummary()
+    {
+        string summary = $"{Date.ToShortDateString()} Running ({Length} min) - Distance: {Distance} miles, Speed: {GetSpeed()} mph, Pace: {GetPace()} min per mile";
+        return summary;
+    }
+}
+
+class StationaryBicycles : Activity
+{
+    public double Speed { get; set; }
+
+    public override double GetDistance()
+    {
+        return Speed * Length / 60;
+    }
+
+    public override double GetSpeed()
+    {
+        return Speed;
+    }
+
+    public override double GetPace()
+    {
+        return 60 / Speed;
+    }
+
+    public override string GetSummary()
+    {
+        string summary = $"{Date.ToShortDateString()} Stationary Bicycles ({Length} min) - Distance: {GetDistance()} miles, Speed: {Speed} mph, Pace: {GetPace()} min per mile";
+        return summary;
+    }
+}
+
+class Swimming : Activity
+{
+    public int Laps { get; set; }
+
+    public override double GetDistance()
+    {
+        return Laps * 50 / 1000.0;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / Length) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return Length / GetDistance();
+    }
+
+    public override string GetSummary()
+    {
+        string summary = $"{Date.ToShortDateString()} Swimming ({Length} min) - Distance: {GetDistance()} km, Speed: {GetSpeed()} kph, Pace: {GetPace()} min per km";
+        return summary;
+    }
+}
+
+class PolymorphismwithExerciseTracking
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        List<Activity> activities = new List<Activity>();
+
+        // Create Running activity
+        Console.WriteLine();
+        Running running = new Running
+        {
+            Date = new DateTime(2022, 11, 3),
+            Length = 30,
+            Distance = 3.0
+        };
+        activities.Add(running);
+
+        // Create Stationary Bicycles activity
+        StationaryBicycles stationaryBicycles = new StationaryBicycles
+        {
+            Date = new DateTime(2022, 11, 3),
+            Length = 30,
+            Speed = 6.0
+        };
+        activities.Add(stationaryBicycles);
+
+        // Create Swimming activity
+        Swimming swimming = new Swimming
+        {
+            Date = new DateTime(2022, 11, 3),
+            Length = 30,
+            Laps = 30
+        };
+        activities.Add(swimming);
+
+        // Display activity summaries
+        foreach (Activity activity in activities)
+
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
+
+// This program is an exercise tracking application that allows users to record and track their activities, 
+// such as running, stationary bicycling, and swimming. It utilizes polymorphism to calculate and display 
+// activity-specific information, including distance, speed, and pace, for each activity.
